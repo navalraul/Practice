4bdeefb89d7849f57052376c3a57b411223fefdb
navalraul <129669942+navalraul@users.noreply.github.com>
2023-04-23 07:30:37 +0530

    update

--- exeample.js
+++ exeample.js
@@ -1,8 +1,117 @@
-var x= 1392769
-var y= 23955223
+// var x= 1392769
+// var y= 23955223
 
-if (x < y) {
-    console.log(x)
-}else {
-    console.log(y)
+// if (x < y) {
+//     console.log(x)
+// }else {
+//     console.log(y)
+// }
+
+
+// var number1 = 20
+// var number2 = 30
+// if (number1 > number2) {
+//     console.log(number1)
+// }else {
+//     console.log(number2)
+// }
+
+
+// Find the largest of three number. (var num1 = 15, var  num2 = 20, var num3 =40)
+
+
+// var num1 = 15
+// var num2 = 20
+// var num3 = 30
+
+// if (num1>= num2 && num2 >= num3) {
+//     console.log(num1)
+// } else if (num2 >= num1 && num2 >= num3) {
+//     console.log(num2)
+// }else {
+//     console.log(num3)
+// }
+
+// var range = 50
+
+// if (5 <= range && 25 <= range) {
+//     console.log(" Number not in range")
+// }else {
+//     console.log("Number is in range")
+// }
+
+
+// Write a program to print numbers from 1 to 10.
+
+// var number1 = 1;
+// var number2 = 10;
+
+// function mynumber (a,b){
+//     for(var i = a; i <= b; i++) {
+//         console.log(i)
+//     }
+
+// }
+// mynumber(number1,number2)
+
+
+// Write a program to calculate the sum of first 10 natural number.
+
+// var sum = 0;
+
+// function natural(a,b) {
+//     for(i = a;i <= b; i++){
+//         sum= sum+i;
+//     }
+//     console.log(sum)
+// }
+// natural(1,15)
+
+
+var num = 3
+var num2 = 10
+function multi(a,b) {
+    for (i = 1;i <=10;i++){
+        console.log(a*i);
+    }
 }
+multi(num,num2)
+
+// var number = 239
+
+// function findnumber(number) {
+//     if (number%2 == 0){
+//         return "number is even"
+//     }else {
+//         return "number is odd"
+//     }
+// }
+// console.log(findnumber(number))
+
+
+// var userage = 85;
+
+// if(userage >80) {
+//     console.log("You are over age dor dl.")
+// }else if (userage >18) {
+//     console.log("You are allowed for dl.")
+// }else if (userage >=18) {
+//     console.log("You are allowed for LL")
+// }else {
+//     console.log("You are under age for dl.")
+// }
+
+// switch(true) {
+//     case (userage>18):
+//         console.log("you are allowed for dl");
+//         break;
+//     case(userage>=18):
+//         console.log("you are allowed for LL.");
+//         break;
+//     case(userage>80):
+//         console.log("you are over aged for dl.");
+//         break;
+//     default:
+//         console.log("default"); 
+
+// }
